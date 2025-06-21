c9d5bcdae0b3e6193b9a6f09ddb2e7f89d659a34
afishafiraa <afishafiraa@gmail.com>
2025-06-21 21:05:51 +0700

    Remove service account JSON and use environment variables

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -1,12 +1,11 @@
 import { google } from 'googleapis';
 import { JWT } from 'google-auth-library';
-import credentials from './credentials/service-account.json';
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
 
 const auth = new JWT({
-  email: credentials.client_email,
-  key: credentials.private_key,
+  email: process.env.GOOGLE_CLIENT_EMAIL,
+  key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   scopes: SCOPES,
 });
 
